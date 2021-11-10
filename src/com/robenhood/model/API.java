e07cf84d0e9bf46cff862e705fdbb036d140e97c
SolvedSphinx126 <bwo0002@gmail.com>
2021-11-10 17:19:49 -0600

    Add API.getCryptoValue

--- src/com/robenhood/model/API.java
+++ src/com/robenhood/model/API.java
@@ -4,15 +4,25 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.time.OffsetDateTime;
 
 
 public class API {
-    public static void main(String[] args) throws MalformedURLException {
-        URL url = new URL("https://api.cryptowat.ch/markets/kraken/btceur/price");
+    static URI uri;
+    public static void main(String[] args) throws URISyntaxException {
+        uri = new URI("https://api.cryptowat.ch/markets/kraken/");
 
+        getCryptoValue(OffsetDateTime.now(), "eth");
+
+    }
+
+    static String fetchAPI(URI uri)
+    {
         String jsonResponse = "";
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"))) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(uri.toURL().openStream(), "UTF-8"))) {
             for (String line; (line = reader.readLine()) != null;) {
                 jsonResponse += line;
             }
@@ -21,6 +31,13 @@ public class API {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.print(jsonResponse);
+        return jsonResponse;
+    }
+
+    public static double getCryptoValue(OffsetDateTime time, String symbol) {
+        URI tempUrl = uri.resolve(symbol + "usd/price");
+        String response = fetchAPI(tempUrl);
+        System.out.println(response);
+        return 0;
     }
 }
