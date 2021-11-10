fc39ae947538f41add2d51ae8d13702fabd1a504
SolvedSphinx126 <bwo0002@gmail.com>
2021-11-10 16:05:21 -0600

    update file structure

--- src/com/robenhood/data/ApiGetter.java
+++ src/com/robenhood/data/ApiGetter.java
@@ -1,27 +1,26 @@
 package com.robenhood.data;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 import java.net.URLConnection;
 
 
 public class ApiGetter {
-    public static void main(String[] args) throws MalformedURLException {
-        URL url = new URL("https://api.cryptowat.ch/markets/kraken/btceur/price");
-
+    public static void main(String[] args) throws MalformedURLException, FileNotFoundException {
+        //File cryptowatchApiKey = new File("../../../Resources/cryptowatchApi.key");
+        //Scanner scan = new Scanner(cryptowatchApiKey);
+        URL url = new URL("https://api.cryptowat.ch/markets/kraken/btceur/price?apikey="/* + scan.nextLine()*/);
         String jsonResponse = "";
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"))) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), StandardCharsets.UTF_8))) {
             for (String line; (line = reader.readLine()) != null;) {
                 jsonResponse += line;
             }
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
         System.out.print(jsonResponse);
+
     }
 }
