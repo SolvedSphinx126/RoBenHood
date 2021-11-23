d2b424339b457ff2fbdb0b843a87710a9099be62
ncw0009 <ncw0009@uah.edu>
2021-11-22 18:23:03 -0600

    Started GUI

--- src/com/robenhood/gui/GUIMain.java
+++ src/com/robenhood/gui/GUIMain.java
@@ -1,9 +1,13 @@
 package com.robenhood.gui;
 
+import com.robenhood.model.Model;
+import com.robenhood.model.Portfolio;
+
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import java.awt.event.*;
+import java.util.*;
 
 public class GUIMain extends JDialog {
     private JPanel contentPane;
@@ -16,22 +20,33 @@ public class GUIMain extends JDialog {
     private JLabel currentTicker;
     private JButton changeTicker;
     private JList tradesList;
+    private JList list1;
     private JButton updateList;
 
     public GUIMain() {
         setTitle("RoBen Hood");
         setContentPane(contentPane);
+        Model userModel = new Model();
         setModal(true);
         getRootPane().setDefaultButton(buttonQuit);
-        String [] ListData = {"Profile 1", "Profile 2", "Profile 3"};
-        listUpdate(ListData);
-        profileLabel.setText(ListData[0]);
-        tradesList.setListData(ListData);
+        ArrayList <String> ListData = new ArrayList<String>();
+        ArrayList <String> TradeData = new ArrayList<String>();
+        ListData.add("Profile 1");
+        ListData.add("Profile 2");
+        ListData.add("Profile 3");
+        profileUpdate(ListData);
+        TradeData.add("Trade1   Value1  Date1   Complete");
+        TradeData.add("Trade2   Value2  Date2   Canceled");
+        TradeData.add("Trade3   Value3  Date3   Incomplete");
+        tradeUpdate(TradeData);
+        profileLabel.setText(ListData.get(0));
+        tradesList.setListData(TradeData.toArray());
 
         profileList.addListSelectionListener(new ListSelectionListener() {
             @Override
             public void valueChanged(ListSelectionEvent e) {
                 String selection = profileList.getSelectedValue().toString();
+                //userModel.setCurrentPortfolio(selection);
                 profileLabel.setText(selection);
             }
         });
@@ -64,8 +79,8 @@ public class GUIMain extends JDialog {
         });
     }
 
-    private void listUpdate(String [] ListData){
-        profileList.setListData(ListData);
+    private void profileUpdate(ArrayList <String> ListData){
+        profileList.setListData(ListData.toArray());
     }
     private void onQuit() {
         // add your code here if necessary
@@ -73,6 +88,10 @@ public class GUIMain extends JDialog {
     }
     private void tickChange(){
         currentTicker.setText(tickerTextField.getText());
+    }
+
+    private void tradeUpdate(ArrayList <String> TradeData){
+        tradesList.setListData(TradeData.toArray());
     }
 
     public static void main(String[] args) {
