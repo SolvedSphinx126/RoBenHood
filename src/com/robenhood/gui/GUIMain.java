e48ebdb989562af4be8e3d90443bff75d498fcae
Jeremiah Rhoton <JeremiahHR133@gmail.com>
2021-11-23 23:00:15 -0600

    Small changes

--- src/com/robenhood/gui/GUIMain.java
+++ src/com/robenhood/gui/GUIMain.java
@@ -13,9 +13,7 @@ import java.time.OffsetDateTime;
 public class GUIMain extends JFrame {
     private JPanel contentPane;
     private JButton buttonQuit;
-    private JTabbedPane tabbedPane1;
     private JList profileList;
-    private JPanel labelPanel;
     private JLabel selectedPort;
     private JButton newPortButton;
     private JButton deletePortButton;
